6868c82e8d9209965e638800ce0da1a14b1ed588
ilmossp <ilyamodeni@gmail.com>
2023-06-06 22:15:47 +0100

    feat: added fonts

--- Best-Pick/tailwind.config.cjs
+++ Best-Pick/tailwind.config.cjs
@@ -1,8 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Work\\ Sans", "Vazirmatn", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: {
           100: "#79a39d",
