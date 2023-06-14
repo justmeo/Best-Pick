8af0885fdc54aaff56cd35ff4d1bda9cfa0890ee
ilmossp <ilyamodeni@gmail.com>
2023-06-14 11:54:03 +0100

    feat: added sliding navbar

--- Best-Pick/tailwind.config.cjs
+++ Best-Pick/tailwind.config.cjs
@@ -1,15 +1,11 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
-
-
-
-
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Work\\ Sans", "Vazirmatn", ...defaultTheme.fontFamily.sans],
+        sans: ["Work\\ Sans", "Archivo", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: {
